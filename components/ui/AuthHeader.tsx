a8e4b71bfd703005069aaf5ba21d2c7b9ef71058
techalbrok <tecnologia.albroksa@gmail.com>
2025-11-15 14:58:18 +0100

    Actualizar rutas de logotipos con imágenes reales

--- components/ui/AuthHeader.tsx
+++ components/ui/AuthHeader.tsx
@@ -7,7 +7,7 @@ interface AuthHeaderProps {
 const AuthHeader: React.FC<AuthHeaderProps> = ({ title }) => {
   return (
     <div className="text-center">
-      <img src="/images/logo_albrok_rojo_transp.png" alt="Albroxfera Logo" className="h-[100px] mx-auto mb-4 block dark:hidden" />
+      <img src="/images/LOGO-SIN-DGS-1.png" alt="Albroksa Logo" className="h-[100px] mx-auto mb-4 block dark:hidden" />
       <img src="/images/logo_albrok_blanco_transp.png" alt="Albroxfera Logo" className="h-[100px] mx-auto mb-4 hidden dark:block" />
       <h1 className="text-2xl font-bold text-gray-900 dark:text-white font-heading">
         {title}
